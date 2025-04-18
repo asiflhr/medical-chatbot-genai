3b163486c205a766e275cd17a0e1819eee18c5e4
Muhammad Asif <asif88383@gmail.com>
2025-04-18 21:49:39 +0500

    Update requirements and add initial setup and trials notebook
    - Updated sentence-transformers version in requirements.txt
    - Added trials.ipynb for data extraction and embedding processes
    - Created setup.py for package configuration
    - Added Medical_book.pdf for data processing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Chatbot",
+    version="0.0.1",
+    description="Generative AI Chatbot",
+    author="Muhammad Asif",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+)
